98e11df7f16fc9ba021b12c5e3ba2ca3c6fdb848
youhuazi <youhuazi@gmail.com>
2022-12-22 18:58:22 +0900

    add laradock

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,16 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+use Illuminate\Support\Facades\Log;
 Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('test', function () {
+    return view('welcome');
+});
+
+Route::post('fromSendGrid', function (\http\Client\Request $request) {
+    Log::info($request->all());
     return view('welcome');
 });
